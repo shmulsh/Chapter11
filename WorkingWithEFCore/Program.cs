90f994ba21c368c54d2242f82511b21bc6eca9ca
Shmulik, Shahar <ss250819@ncr.com>
2018-07-18 11:03:58 +0300

    11

--- WorkingWithEFCore/Program.cs
+++ WorkingWithEFCore/Program.cs
@@ -1,12 +1,58 @@
 ﻿using System;
-
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using static System.Console;
+using Packt.CS7;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 namespace WorkingWithEFCore
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            QueryingCategories();
+            QueryingProducts();
         }
-    }
+        static void QueryingCategories()
+        {
+            using (var db = new Northwind())
+            {
+                var loggerFactory = db.GetService<ILoggerFactory>();
+                loggerFactory.AddProvider(new ConsoleLoggerProvider());
+                WriteLine("Categories and how many products they have:");
+                // a query to get all categories and their related products
+                IQueryable<Category> cats = db.Categories.Include(c => c.Products);
+                foreach (Category c in cats)
+                {
+                    WriteLine($"{c.CategoryName} has {c.Products.Count} products.");
+                }
+            }
+        }
+        static void QueryingProducts()
+        {
+            using (var db = new Northwind())
+            {
+                var loggerFactory = db.GetService<ILoggerFactory>();
+                loggerFactory.AddProvider(new ConsoleLoggerProvider());
+                WriteLine("Products that cost more than a price, and sorted.");
+                string input;
+                decimal price;
+                do
+                {
+                    Write("Enter a product price: ");
+                    input = ReadLine();
+                } while (!decimal.TryParse(input, out price));
+                IQueryable<Product> prods = db.Products
+                .Where(product => product.Cost > price)
+                .OrderByDescending(product => product.Cost);
+                foreach (Product item in prods)
+                {
+                    WriteLine($"{item.ProductID}: {item.ProductName} costs {item.Cost:$#,##0.00} and  has { item.Stock} units in stock.");
+                    }
+                }
+            }
+        }
+
 }
